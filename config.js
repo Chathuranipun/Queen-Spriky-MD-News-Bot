3f83905c442170e79cac1f6c7e5ec2b0df739727
Chathuranipun <122404220+Chathuranipun@users.noreply.github.com>
2024-09-29 20:33:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
-    MONGODB: process.env.MONGODB || '',    // Enter Your MongoDB URL
-    Owner: process.env.OwnerNumber || '94758900210',    // Enter Your Owner Number
-    BotNumber: process.env.BotNumber || '94773366833'    // Enter Your Bot Number
+    SESSION_ID: process.env.SESSION_ID || 'KFFmgKDB#PfHWRuzJ6v3WMJsI6CTp-cpl1ki5yECz4UEwk4vLoBo', // Enter Your Session ID
+    MONGODB: process.env.MONGODB || 'https://cloud.mongodb.com/v2#/org/66f962440886094f0a4ef346/projects',    // Enter Your MongoDB URL
+    Owner: process.env.OwnerNumber || '94784760582',    // Enter Your Owner Number
+    BotNumber: process.env.BotNumber || '94784760582'    // Enter Your Bot Number
 };
